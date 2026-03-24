0afeaf756605b1317854af362b61befedb7bdc8d
Shammas Khan <128974001+shammaskhann@users.noreply.github.com>
2026-03-25 03:45:29 +0500

    🚀 Deploy Web Build to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2899930919"
+    serviceWorkerVersion: "3967004205"
   }
 });
